de0fd72fe0d2680d20c0df3fd22b2ba415cdc0aa
Nick Boucart <nboucart@gmail.com>
2012-01-05 10:52:34 +0100

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpensaasDemo::Application.config.secret_token = '5ac942b9023b48455c0ac2ed174d4040645203af63387efb60ecc23cb0aaa738ce08bf7a5a4a62fc57e15bb519440fcd0a5e60faeb232c72c18ec9828299cdcb'
